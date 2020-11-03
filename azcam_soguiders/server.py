f5e087694e9612e0520e17db7d0c00099344b18b
Michael Lesser <mlesser@arizona.edu>
2020-11-03 06:19:40 -0700

    cleanup

--- azcam_soguiders/server.py
+++ azcam_soguiders/server.py
@@ -6,21 +6,21 @@ import os
 import subprocess
 import sys
 
-from azcam_ds9.ds9display import Ds9Display
-from azcam_mag.controller_mag import ControllerMag
-from azcam_mag.exposure_mag import ExposureMag
-from azcam_mag.tempcon_mag import TempConMag
-from azcam_mag.udpinterface import UDPinterface
-from azcam_soguiders.detectors import detector_ccd57
-
+from azcam.server import azcam
 import azcam
 import azcam.monitorinterface
 import azcam.shortcuts
 from azcam.cmdserver import CommandServer
 from azcam.genpars import GenPars
 from azcam.instrument import Instrument
-from azcam.server import azcam
 from azcam.telescope import Telescope
+
+from azcam_ds9.ds9display import Ds9Display
+from azcam_mag.controller_mag import ControllerMag
+from azcam_mag.exposure_mag import ExposureMag
+from azcam_mag.tempcon_mag import TempConMag
+from azcam_mag.udpinterface import UDPinterface
+from azcam_soguiders.detectors import detector_ccd57
 
 azcam.log("Loading azcam-soguiders environment")
 
