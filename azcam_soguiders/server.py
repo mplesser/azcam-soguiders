d562613008b61ff053a6151448e59873ec9b3d24
Michael Lesser <mlesser@arizona.edu>
2020-11-22 15:51:55 -0700

    cleanup

--- azcam_soguiders/server.py
+++ azcam_soguiders/server.py
@@ -79,8 +79,8 @@ azcam.db.datafolder = azcam.utils.fix_path(azcam.db.datafolder)
 # enable logging
 # ****************************************************************
 tt = datetime.datetime.strftime(datetime.datetime.now(), "%d%b%y_%H%M%S")
-azcam.db.logfile = os.path.join(azcam.db.datafolder, "logs", f"server_{tt}.log")
-azcam.logging.start_logging(azcam.db.logfile, "123")
+azcam.db.logger.logfile = os.path.join(azcam.db.datafolder, "logs", f"server_{tt}.log")
+azcam.db.logger.start_logging(azcam.db.logger.logfile, "123")
 azcam.log(f"Configuring {azcam.db.systemname}")
 
 # ****************************************************************
