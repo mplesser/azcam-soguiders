ccf4dc30d401d05dc12d662441f5be414a8cb972
Michael Lesser <mlesser@arizona.edu>
2021-03-10 09:10:23 -0700

    cleanup

--- azcam_soguiders/console_soguiders.py
+++ azcam_soguiders/console_soguiders.py
@@ -17,7 +17,7 @@ from azcam_ds9.ds9display import Ds9Display
 azcam.db.systemname = "soguiders"
 azcam.db.systemfolder = os.path.dirname(__file__)
 azcam.db.datafolder = azcam.db.systemfolder
-parfile = os.path.join(azcam.db.datafolder, f"parameters_{azcam.db.systemname}.ini")
+parfile = os.path.join(azcam.db.datafolder, f"parameters_console_{azcam.db.systemname}.ini")
 
 # ****************************************************************
 # add folders to search path
